a8df08e17fb217be150549939f70efbc6d0d6147
Amjad Ghanimah <thegridhive@plbee.com>
2025-07-02 15:09:13 +0300

    hero edits

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,33 +12,34 @@ const Hero: React.FC = () => {
       id="hero"
       className="
         relative h-screen overflow-hidden bg-black
-        bg-[url('../assets/honeycomb-pattern.png')]
-        bg-cover bg-center
+        bg-[url('/assets/honeycomb-pattern.png')] bg-cover bg-center
       "
     >
       {/* Headline */}
       <div
         className={`
-          absolute inset-x-0 top-1/4
-          px-4 sm:px-6 lg:px-8
-          text-center
-          transform transition-all duration-700
+          absolute
+          inset-x-0
           ${mounted ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-5'}
+          top-20 sm:top-32
+          px-4 sm:px-12 lg:px-24
+          transform transition-all duration-700
+          text-center sm:text-left
         `}
       >
-        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-extrabold text-white leading-tight">
+        <h1 className="text-3xl sm:text-5xl font-extrabold text-white leading-snug">
           We Build Hives.
           <br />
-          <span className="inline-block ml-0 sm:ml-4">We Gain Trust.</span>
+          <span className="inline-block sm:ml-8">We Gain Trust.</span>
         </h1>
       </div>
 
-      {/* Buttons */}
+      {/* React-Router Links */}
       <div
         className={`
-          absolute inset-x-0 top-1/2
-          flex flex-col sm:flex-row justify-center items-center
-          space-y-4 sm:space-y-0 sm:space-x-6
+            className={
+          absolute left-1/2 top-[60%] -translate-x-1/2
+          flex space-x-6 z-10
           transform transition-opacity duration-700
           ${mounted ? 'opacity-100' : 'opacity-0'}
         `}
@@ -46,11 +47,11 @@ const Hero: React.FC = () => {
         <Link
           to="/build-hive"
           className="
-            px-6 py-3 sm:px-8 sm:py-4
+            px-6 py-2 sm:px-8 sm:py-3
             bg-yellow-400 text-black font-semibold rounded-full
             transform transition duration-500 ease-out
             hover:-translate-y-1 hover:shadow-lg
-            w-full sm:w-auto text-center
+            text-center w-3/4 sm:w-auto
           "
         >
           Build Your Hive
@@ -58,11 +59,11 @@ const Hero: React.FC = () => {
         <Link
           to="/join-hive"
           className="
-            px-6 py-3 sm:px-8 sm:py-4
+            px-6 py-2 sm:px-8 sm:py-3
             bg-transparent border-2 border-yellow-400 text-yellow-400 font-semibold rounded-full
             transform transition duration-500 ease-out
             hover:bg-yellow-400 hover:text-black hover:shadow-lg
-            w-full sm:w-auto text-center
+            text-center w-3/4 sm:w-auto
           "
         >
           Join Your Hive
